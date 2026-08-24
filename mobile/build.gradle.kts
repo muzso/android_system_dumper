ed1bed1f47f8faab02931a130a8de78b14529d9a
Zsolt Müller <zsolt.muller@gmail.com>
2026-08-24 23:58:40 +0200

    - updated versionCode and versionName

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "hu.muzso.android_system_dumper.mobile"
     minSdk = 26
     targetSdk = 37
-    versionCode = 1
-    versionName = "1.0.0"
+    versionCode = 2
+    versionName = "1.0.1"
 
     testInstrumentationRunner = "hu.muzso.android_system_dumper.HiltTestRunner"
   }
